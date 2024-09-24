f6280cef7c4c602125d2b991771455fdc2782a05
FeliBlanco <felipeblanco09@hotmail.com>
2024-09-24 17:00:34 -0300

    dsadas

--- src/routes/Login/index.jsx
+++ src/routes/Login/index.jsx
@@ -2,11 +2,11 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Login() {
 
-    const { loginWithRedirect } = useAuth0();
+    const { loginWithPopup } = useAuth0();
     
     return (
         <div>
-            <button onClick={() => loginWithRedirect()}>Log In</button>
+            <button onClick={() => loginWithPopup()}>Log In</button>
         </div>
     )
 }
